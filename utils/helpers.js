539fd1329f06d09abe8fe5497045c3574471f2bb
Daniel Stallworth <danielstallworthmit@gmail.com>
2017-11-15 18:45:27 -0800

    Sample deck data added to utils/helpers

---
+++ utils/helpers.js
@@ -0,0 +1,31 @@
+
+export function getDeckInfo (deck) {
+    const info = {
+        React : {
+            title: 'React',
+            questions: [
+                {
+                    question: 'What is React?',
+                    answer: 'A library for managing user interfaces'
+                },
+                {
+                    question: 'Where do you make Ajax requests in React?',
+                    answer: 'The componentDidMount lifecycle method'
+                }
+            ]
+        },
+        Javascript : {
+            title: 'Javascript',
+            questions: [
+                {
+                    question: 'What is a clojure?',
+                    answer: 'The combination of a function and the lexical environment within which that function was declared'
+                },
+                {
+                    question: 'Is Javascript Cool?',
+                    answer: 'Yes, it is very cool'
+                }
+            ]
+        },
+    }
+}
