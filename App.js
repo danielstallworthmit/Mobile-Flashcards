366918a5e80f19e0437f45384dd3cfdb62e1c785
Daniel Stallworth <danielstallworthmit@gmail.com>
2017-11-20 14:17:27 -0800

    Add card component to separate decks on the list

--- App.js
+++ App.js
@@ -3,21 +3,6 @@ import { StyleSheet, Text, View, StatusBar, Dimensions } from 'react-native'
 import { StackNavigator } from 'react-navigation'
 import { Constants } from 'expo'
 import DeckList from './src/components/DeckList'
-// import DeckView from './components/DeckView'
-// import QuizViewQuestion from './components/QuizViewQuestion'
-// import QuizViewAnswer from './components/QuizViewAnswer'
-// import NewDeck from './components/NewDeck'
-// import NewQuestion from './components/NewQuestion'
-
-// const DeckNavigator = StackNavigator({
-//   StatusBarMod: { screen: StatusBarMod },
-//   DeckList: { screen: DeckList },
-//   DeckView: { screen: DeckView },
-//   QuizViewQuestion: { screen: QuizViewQuestion },
-//   QuizViewAnswer: { screen: QuizViewAnswer },
-//   NewDeck: { screen: NewDeck },
-//   NewQuestion: { screen: NewQuestion }
-// })
 
 export default class App extends React.Component {
   render() {
@@ -33,8 +18,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
     paddingTop:Constants.statusBarHeight
   }
 });
--- src/components/DeckList.js
+++ src/components/DeckList.js
@@ -1,32 +1,24 @@
 import React from 'react'
 import { Text, View, ListView, FlatList } from 'react-native'
 import { getDeckInfo } from '../../utils/helpers'
+import Card from './Card'
 
 function DeckItem({title, questions}) {
     return (
-        <View style={{flex:1}}>
-            <Text>{title}</Text>
-            <Text>{`${questions.length} cards`}</Text>
-        </View>
+        <Card style={{flex:1}}>
+                <Text style={{fontSize: 25}}>{title}</Text>
+                <Text>{`${questions.length} cards`}</Text>
+        </Card>
     )
 }
 
-// export default DeckList = () => (<View><Text>DeckList</Text></View>)
 export default class DeckList extends React.Component {
-    // componentDidMount() {
-    //     const ds = new ListView.DataSource({
-    //         rowHasChanged: (r1,r2) => r1 !== r2
-    //     });
-    //     this.dataSource = ds.cloneWithRows()
-    // }
     renderItem = ({item}) => {
-        // <Text>{item}</Text>
         return <DeckItem key={item.title} {...item} /> 
     }
     render() {
         return (
             <View>
-                <Text>Hi</Text>
                 <FlatList data={getDeckInfo()} renderItem={this.renderItem} keyExtractor={(item) => item.title} />
             </View>
         )
